aa9cd8882c305826c942c279179c19a063999c61
nathanziri <nathanziri@hotmail.com>
2021-10-03 09:12:00 -0400

    updated deck

--- Deck.h
+++ Deck.h
@@ -4,9 +4,16 @@
 
 #ifndef COMP_345_PROJ_DECK_H
 #define COMP_345_PROJ_DECK_H
-
+#include <vector>
+#include "Card.h"
 
 class Deck {
+    public:
+        std::vector<Card> deckOfCards;
+    Deck();
+    Deck(int numPlayers);
+    void Deal(Deck otherDeck);
+    void Play(Deck otherDeck);
 
 };
 
