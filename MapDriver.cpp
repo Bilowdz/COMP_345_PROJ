54f6ee5686c192fb605f011971021bf2759ca1de
nicolo <Nicolo_Tabar@outlook.com>
2021-10-08 13:16:33 -0400

    Fixed current directory issue (MapDriver.cpp)

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "Map.h"
-
 int main() {
+
     MapLoader* loader(new MapLoader);
-    Map *map = loader->Load("A:\\Programming Stuff\\GitHub\\COMP_345_PROJ\\Maps\\canada.map", false);
-    // map->Print();
+    //Map *map = loader->Load("C:\\Users\\Nicolo pt 2\\Desktop\\School\\University\\6. FALL2021\\COMP345\\Projects\\Project\\Syllabus.COMP345.paquet.2021.2.pdf\\COMP_345_PROJ\\Maps\\canada.map", false);
+    Map *map = loader->Load("../Maps/canada.map", false);
+    map->Print();
     std::cout << map->Validate();
 }
